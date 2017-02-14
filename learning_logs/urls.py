5343accdc733b0ccfe6511d2cd362580967abea8
Hao Ruan <haoru@cisco.com>
2017-02-14 18:20:44 +0800

    classic query flow

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -6,4 +6,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^topics/$', views.topics, name='topics'),
+    url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
+
 ]
