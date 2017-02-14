431835b4982c4991bac77df30581119863ef25af
Hao Ruan <haoru@cisco.com>
2017-02-14 15:48:31 +0800

    create models

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -9,3 +9,15 @@ class Topic(models.Model):
     def __str__(self):
         # short information about this model
         return self.text
+
+class Entry(models.Model):
+    topic = models.ForeignKey(Topic)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    # 用于管理模型的额外信息
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        return self.text[:50] + '...'
