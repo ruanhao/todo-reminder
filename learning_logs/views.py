f7e233ade562bf2e8e8229e4daade99dc132c8f3
Hao Ruan <haoru@cisco.com>
2017-02-16 10:45:27 +0800

    implement add topic and entry

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+
 from .models import Topic
+from .forms import TopicForm, EntryForm
+
 # Create your views here.
 
 def index(request):
@@ -16,3 +21,29 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')  # -: descend
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    if request.method != 'POST':
+        form = TopicForm()
+    else:
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learning_logs:topics'))
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
+
+def new_entry(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+    if request.method != 'POST': # 未提交数据,创建一个空表单
+        form = EntryForm()
+    else:
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return HttpResponseRedirect(reverse('learning_logs:topic',
+                                                args=[topic_id]))
+    context = {'topic': topic, 'form': form}
+    return render(request, 'learning_logs/new_entry.html', context)
